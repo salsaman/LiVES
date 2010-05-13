bc33e4150bfa86de0ba141bdd7397390115e2a1d
gfinch <salsaman@gmail.com>
2010-05-13 09:44:39 +0000

    code correctness

--- lives-plugins/weed-plugins/livetext.c
+++ lives-plugins/weed-plugins/livetext.c
@@ -12,7 +12,7 @@
 
 ///////////////////////////////////////////////////////////////////
 
-static int num_versions=1; // number of different weed api versions supported
+static int num_versions=2; // number of different weed api versions supported
 static int api_versions[]={131,100}; // array of weed api versions supported in plugin, in order of preference (most preferred first)
 
 static int package_version=2; // version of this package

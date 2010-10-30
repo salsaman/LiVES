c5d52d11d477b9319fb344a9e959aceb1e6c972b
gfinch <gfinch@667af9e9-3036-4fcf-8332-edbcb1dde82b>
2010-10-30 14:57:06 +0000

    update

--- trunk/lives-plugins/weed-plugins/slide_over.c
+++ trunk/lives-plugins/weed-plugins/slide_over.c
@@ -37,8 +37,7 @@ static inline uint32_t fastrand(void)
 }
 
 
-inline int pick_direction(int dirpref, weed_timecode_t tc) {
-  if (dirpref!=0) return dirpref;
+inline int pick_direction (weed_timecode_t tc) {
   fastrand_val=tc&0xFFFFFFFF;
   return ((fastrand()>>24)&0x03)+1;
 }
@@ -46,7 +45,16 @@ inline int pick_direction(int dirpref, weed_timecode_t tc) {
 
 
 int sover_init (weed_plant_t *inst) {
-  weed_set_int_value(inst,"plugin_direction",-1);
+  int dirpref,error;
+  weed_plant_t **in_params=weed_get_plantptr_array(inst,"in_parameters",&error);
+
+  if (weed_get_boolean_value(in_params[1],"value",&error)==WEED_TRUE) dirpref=0;
+  else if (weed_get_boolean_value(in_params[2],"value",&error)==WEED_TRUE) dirpref=1; // left to right
+  else if (weed_get_boolean_value(in_params[3],"value",&error)==WEED_TRUE) dirpref=2; // right to left
+  else if (weed_get_boolean_value(in_params[4],"value",&error)==WEED_TRUE) dirpref=3; // top to bottom
+  else dirpref=4; // bottom to top
+
+  weed_set_int_value(inst,"plugin_direction",dirpref);
   return WEED_NO_ERROR;
 }
 
@@ -68,33 +76,26 @@ int sover_process (weed_plant_t *inst, weed_timecode_t timecode) {
   register int j;
 
   int transval;
-  int dirpref,dirn;
+  int dirn;
   int mvlower;
   int bound;
 
   in_params=weed_get_plantptr_array(inst,"in_parameters",&error);
   transval=weed_get_int_value(in_params[0],"value",&error);
-
-  if (weed_get_boolean_value(in_params[1],"value",&error)==WEED_TRUE) dirpref=0; // random
-  else if (weed_get_boolean_value(in_params[2],"value",&error)==WEED_TRUE) dirpref=1; // left to right
-  else if (weed_get_boolean_value(in_params[3],"value",&error)==WEED_TRUE) dirpref=2; // right to left
-  else if (weed_get_boolean_value(in_params[4],"value",&error)==WEED_TRUE) dirpref=3; // top to bottom
-  else dirpref=4; // bottom to top
-
   dirn=weed_get_int_value(inst,"plugin_direction",&error);
+  mvlower=weed_get_boolean_value(in_params[6],"value",&error);
+  mvupper=weed_get_boolean_value(in_params[7],"value",&error);
 
-  if (dirn==-1||(dirpref==0&&(transval==0||transval==255))) {
-    dirn=pick_direction(dirpref,timecode);
+  if (dirn==0) {
+    dirn=pick_direction(timecode); // random
     weed_set_int_value(inst,"plugin_direction",dirn);
   }
-
-  mvlower=weed_get_boolean_value(in_params[6],"value",&error);
 
   switch (dirn) {
   case 3:
     // top to bottom
     bound=(float)height*(1.-transval/255.);
-    src1+=irowstride1*(height-bound);
+    if (mvupper) src1+=irowstride1*(height-bound);
     for (j=0;j<bound;j++) {
       weed_memcpy(dst,src1,width*3);
       src1+=irowstride1;
@@ -111,6 +112,7 @@ int sover_process (weed_plant_t *inst, weed_timecode_t timecode) {
     // bottom to top
     bound=(float)height*(transval/255.);
     if (mvlower) src2+=irowstride2*(height-bound);
+    if (!mvupper) src1+=irowstride1*(height-bound);
     for (j=0;j<bound;j++) {
       weed_memcpy(dst,src2,width*3);
       src2+=irowstride2;
@@ -127,7 +129,7 @@ int sover_process (weed_plant_t *inst, weed_timecode_t timecode) {
     bound=(float)width*(1.-transval/255.);
     for (j=0;j<height;j++) {
       weed_memcpy(dst+bound*3,src2+bound*3*!mvlower,(width-bound)*3);
-      weed_memcpy(dst,src1+(width-bound)*3,bound*3);
+      weed_memcpy(dst,src1+(width-bound)*3*mvupper,bound*3);
       src1+=irowstride1;
       src2+=irowstride2;
       dst+=orowstride;
@@ -138,7 +140,7 @@ int sover_process (weed_plant_t *inst, weed_timecode_t timecode) {
     bound=(float)width*(transval/255.);
     for (j=0;j<height;j++) {
       weed_memcpy(dst,src2+(width-bound)*3*mvlower,bound*3);
-      weed_memcpy(dst+bound*3,src1,(width-bound)*3);
+      weed_memcpy(dst+bound*3,src1+!mvupper*bound*3,(width-bound)*3);
       src1+=irowstride1;
       src2+=irowstride2;
       dst+=orowstride;
@@ -159,7 +161,7 @@ weed_plant_t *weed_setup (weed_bootstrap_f weed_boot) {
     int palette_list[]={WEED_PALETTE_BGR24,WEED_PALETTE_RGB24,WEED_PALETTE_END};
     weed_plant_t *in_chantmpls[]={weed_channel_template_init("in channel 0",0,palette_list),weed_channel_template_init("in channel 1",0,palette_list),NULL};
     weed_plant_t *out_chantmpls[]={weed_channel_template_init("out channel 0",0,palette_list),NULL};
-    weed_plant_t *in_params[]={weed_integer_init("amount","Transition _value",128,0,255),weed_radio_init("dir_rand","_Random",1,1),weed_radio_init("dir_l2r","_Left to right",0,1),weed_radio_init("dir_r2l","_Right to left",0,1),weed_radio_init("dir_t2b","_Top to bottom",0,1),weed_radio_init("dir_b2t","_Bottom to top",0,1),weed_switch_init("mlower","_Move lower clip",0),NULL};
+    weed_plant_t *in_params[]={weed_integer_init("amount","Transition _value",128,0,255),weed_radio_init("dir_rand","_Random",1,1),weed_radio_init("dir_r2l","_Right to left",0,1),weed_radio_init("dir_l2r","_Left to right",0,1),weed_radio_init("dir_b2t","_Bottom to top",0,1),weed_radio_init("dir_t2b","_Top to bottom",0,1),weed_switch_init("mlower","_Slide lower clip",1),weed_switch_init("mupper","_Slide upper clip",0),NULL};
 
     weed_plant_t *filter_class=weed_filter_class_init("slide over","salsaman",1,0,&sover_init,&sover_process,NULL,in_chantmpls,out_chantmpls,in_params,NULL);
 
@@ -171,6 +173,12 @@ weed_plant_t *weed_setup (weed_bootstrap_f weed_boot) {
     weed_set_string_array(gui,"rfx_strings",7,rfx_strings);
 
     weed_set_boolean_value(in_params[0],"transition",WEED_TRUE);
+
+    weed_set_int_value(in_params[1],"flags",WEED_PARAMETER_REINIT_ON_VALUE_CHANGE);
+    weed_set_int_value(in_params[2],"flags",WEED_PARAMETER_REINIT_ON_VALUE_CHANGE);
+    weed_set_int_value(in_params[3],"flags",WEED_PARAMETER_REINIT_ON_VALUE_CHANGE);
+    weed_set_int_value(in_params[4],"flags",WEED_PARAMETER_REINIT_ON_VALUE_CHANGE);
+    weed_set_int_value(in_params[5],"flags",WEED_PARAMETER_REINIT_ON_VALUE_CHANGE);
 
     weed_plugin_info_add_filter_class (plugin_info,filter_class);
 

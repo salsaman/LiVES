d56c7179e3f2eaa65dadd5f66dc10edb75534048
gfinch <salsaman@gmail.com>
2012-01-17 05:51:15 +0000

    out params config

--- lives-plugins/weed-plugins/blank_frame_detector.c
+++ lives-plugins/weed-plugins/blank_frame_detector.c
@@ -10,13 +10,12 @@
 #include <weed/weed.h>
 #include <weed/weed-palettes.h>
 #include <weed/weed-effects.h>
-#include <weed/weed-plugin.h>
 #else
 #include "../../libweed/weed.h"
 #include "../../libweed/weed-palettes.h"
 #include "../../libweed/weed-effects.h"
-#include "../../libweed/weed-plugin.h"
 #endif
+
 
 ///////////////////////////////////////////////////////////////////
 
@@ -29,9 +28,14 @@ static int package_version=1; // version of this package
 
 #ifdef HAVE_SYSTEM_WEED
 #include <weed/weed-utils.h> // optional
-#include <weed/weed-plugin-utils.h> // optional
 #else
 #include "../../libweed/weed-utils.h" // optional
+#endif
+
+
+#ifdef HAVE_SYSTEM_WEED_PLUGIN_UTILS
+#include <weed/weed-plugin-utils.h> // optional
+#else
 #include "../../libweed/weed-plugin-utils.h" // optional
 #endif
 

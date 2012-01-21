148477fd6686b59abaaecd1d76d065d516f42703
gfinch <salsaman@gmail.com>
2012-01-21 13:39:48 +0000

    attempt better fix for prefs window

--- src/preferences.c
+++ src/preferences.c
@@ -1774,12 +1774,12 @@ void on_prefDomainChanged(GtkTreeSelection *widget, gpointer dummy)
     }
   }
 
-#ifdef PREF_FORCE_CENTER
-  // this is needed to force the window to update in some cases
-  if (GTK_WIDGET_VISIBLE(prefsw->prefs_dialog)) gtk_widget_show_all(prefsw->prefs_dialog);
-  gtk_widget_queue_draw(prefsw->prefs_dialog);
-  if (dummy==NULL) on_prefDomainChanged(widget,GINT_TO_POINTER(1));
-#endif
+  if (!strcmp(prefs->wm,"Compiz")) {
+    // this is needed to force the window to update in some cases
+    if (GTK_WIDGET_VISIBLE(prefsw->prefs_dialog)) gtk_widget_show_all(prefsw->prefs_dialog);
+    gtk_widget_queue_draw(prefsw->prefs_dialog);
+    if (dummy==NULL) on_prefDomainChanged(widget,GINT_TO_POINTER(1));
+  }
 
   gtk_widget_queue_draw(prefsw->prefs_dialog);
 
@@ -5911,9 +5911,7 @@ _prefsw *create_prefs_dialog (void) {
   g_list_free_strings (audp);
   g_list_free (audp);
 
-#ifndef PREF_FORCE_CENTER
   on_prefDomainChanged(prefsw->selection,NULL);
-#endif
 
   return prefsw;
 }
@@ -5933,11 +5931,11 @@ on_preferences_activate(GtkMenuItem *menuitem, gpointer user_data)
   prefsw = create_prefs_dialog();
   gtk_widget_show(prefsw->prefs_dialog);
 
-#ifdef PREF_FORCE_CENTER
-  // need to do exactly this, else the window does not get properly centered on some WMs
-  while (g_main_context_iteration(NULL,FALSE));
-  on_prefDomainChanged(prefsw->selection,NULL);
-#endif
+  if (!strcmp(prefs->wm,"Compiz")) {
+    // need to do exactly this, else the window does not get properly centered on some WMs
+    while (g_main_context_iteration(NULL,FALSE));
+    on_prefDomainChanged(prefsw->selection,NULL);
+  }
 
 }
 

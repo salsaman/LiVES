ddc759d0d99dfbf15c98d47578cf34b665e5bf5e
gfinch <gfinch@667af9e9-3036-4fcf-8332-edbcb1dde82b>
2013-11-17 14:24:55 +0000

    updates

--- trunk/src/utils.c
+++ trunk/src/utils.c
@@ -1597,9 +1597,11 @@ int calc_new_playback_position(int fileno, uint64_t otc, uint64_t *ntc) {
     calc_aframeno(fileno);
   }
 
-  if (mainw->playing_file==fileno) {
+  if (mainw->playing_file==fileno&&!mainw->clip_switched) {
     last_frame=(mainw->playing_sel&&!mainw->is_rendering)?sfile->end:mainw->play_end;
+    if (last_frame>sfile->frames) last_frame=sfile->frames;
     first_frame=mainw->playing_sel?sfile->start:mainw->play_start;
+    if (first_frame>sfile->frames) first_frame=sfile->frames;
   }
   else {
     last_frame=sfile->frames;

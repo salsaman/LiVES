c3dfb1a444788bcfe52787cc2379e18799642bf6
gfinch <salsaman@gmail.com>
2014-09-18 06:46:21 +0000

    updates

--- lives-plugins/plugins/playback/video/openGL_trickery.cpp
+++ lives-plugins/plugins/playback/video/openGL_trickery.cpp
@@ -1026,10 +1026,10 @@ static boolean Upload(int width, int height) {
       glBegin (GL_QUADS);
       
       glTexCoord2f (0.0, 0.0);
-      glVertex3f (-1.0, 1.1, 0.0);
+      glVertex3f (-1.0, 1.0, 0.0);
       
       glTexCoord2f (1.0, 0.0);
-      glVertex3f (1.0, 1.1, 0.0);
+      glVertex3f (1.0, 1.0, 0.0);
       
       glTexCoord2f (1.0, 1.0);
       glVertex3f (1.0, -1.0, 0.0);

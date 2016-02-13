47d7b5d6b91611620ca63d024c1f2dd4f9d50af2
gfinch <salsaman@gmail.com>
2016-02-13 16:20:31 +0000

    oops

--- src/dialogs.c
+++ src/dialogs.c
@@ -2467,9 +2467,9 @@ boolean do_comments_dialog(int fileno, char *filename) {
       lives_snprintf(sfile->author,256,"%s",lives_entry_get_text(LIVES_ENTRY(commentsw->author_entry)));
       lives_snprintf(sfile->comment,256,"%s",lives_entry_get_text(LIVES_ENTRY(commentsw->comment_entry)));
 
-      save_clip_value(fileno,CLIP_DETAILS_FRAMES,sfile->title);
-      save_clip_value(fileno,CLIP_DETAILS_FRAMES,sfile->author);
-      save_clip_value(fileno,CLIP_DETAILS_FRAMES,sfile->comment);
+      save_clip_value(fileno,CLIP_DETAILS_TITLE,sfile->title);
+      save_clip_value(fileno,CLIP_DETAILS_AUTHOR,sfile->author);
+      save_clip_value(fileno,CLIP_DETAILS_COMMENT,sfile->comment);
 
       if (encoding&&sfile->subt!=NULL&&lives_toggle_button_get_active(LIVES_TOGGLE_BUTTON(commentsw->subt_checkbutton))) {
         char *ext=get_extension(lives_entry_get_text(LIVES_ENTRY(commentsw->subt_entry)));

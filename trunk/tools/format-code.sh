361c4486c2e18ae9573393e4aeb552be13972bc5
gfinch <gfinch@667af9e9-3036-4fcf-8332-edbcb1dde82b>
2017-08-16 12:19:25 +0000

    updates

--- trunk/tools/format-code.sh
+++ trunk/tools/format-code.sh
@@ -1,2 +1,2 @@
-find . -name \*.c -or -name \*.h -or -name \*.cpp -or -name \*.hpp | xargs astyle --style=java -H -Y -s2 -U -k3 -W3 -xC140 -xL -p -U -k3
+find . -name \*.c -or -name \*.h -or -name \*.cpp -or -name \*.hpp | xargs astyle --style=java -H -Y -s2 -U -k3 -W3 -xC140 -xL -p
 #astyle --style=java -H -Y -U -k3 -W3 -xC140 -xL -p -U -k3 -xw smogrify
